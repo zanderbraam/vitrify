5ccd1bcf85956ca721dceb4e3d2e62bf89f718ce
zanderbraam <zander@nmrql.com>
2019-06-22 13:48:05 +0200

    Added more classes for the models

---
+++ src/models/multi_layer_perceptron.py
@@ -0,0 +1,26 @@
+
+
+class MultiLayerPerceptron:
+    """
+    TODO comment
+    """
+    def __init__(self, name, *args, **kwargs):
+        """
+        TODO comment
+        :param args:
+        :param kwargs:
+        """
+        self.name = name
+        # TODO load kwargs
+
+    def train(self, x_train, y_train, x_valid, y_valid):
+        pass
+
+    def evaluate(self, x_test, y_test):
+        pass
+
+    def save(self, path: str):
+        pass
+
+    def predict(self, x):
+        pass
---
+++ src/models/variational_autoencoder.py
@@ -0,0 +1,27 @@
+
+
+class VariationalAutoEncoder:
+    """
+    TODO comment
+    """
+    def __init__(self, name, *args, **kwargs):
+        """
+        TODO comment
+
+        :param args:
+        :param kwargs:
+        """
+        self.name = name
+        # TODO load kwargs
+
+    def train(self, x_train, x_valid):
+        pass
+
+    def evaluate(self, x_test):
+        pass
+
+    def save(self, path: str):
+        pass
+
+    def sample(self, n: int):
+        pass
